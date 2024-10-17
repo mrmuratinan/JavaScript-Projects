0c56ef0d225f8f9147db72c8e624e16f5e16e0da
mrmuratinan <mrmuratinan@gmail.com>
2024-10-17 11:54:04 -0400

    commit

--- Basic-JavaScript-Projects/Project2_functions/JS/main.js
+++ Basic-JavaScript-Projects/Project2_functions/JS/main.js
@@ -1,9 +1,12 @@
+// changing the element innerHTML by clicking the button
 function myFirstFunction() {
 	var strP = 'This is the changed text after clicking';
 	var str = 'This is the button text';
 	document.getElementById('Button_Text').innerHTML = str;
 	document.getElementById('paragraph').innerHTML = strP;
 }
+
+// Change the element innerHTML by concatenating the strings
 function myConcatenate() {
 	var str1 = 'I am learning';
 	str1 += ' a lot from this book!';
